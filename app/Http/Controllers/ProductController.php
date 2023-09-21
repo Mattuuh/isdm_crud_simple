7953a1d6df8c180b7c2604ff89a34139de6b0e9a
Mattuuh <99518568+Mattuuh@users.noreply.github.com>
2023-09-20 21:09:41 -0300

    Agregado metodos destroy y show ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,9 +15,28 @@ class ProductController extends Controller
             'products' => DB::table('products')->paginate(10)
         ]);
     }
+    
     public function create()
     {
         return view(('products.create'));
+    }
+    
+    public function destroy($id)
+    {
+        //Busqueda del producto
+        $product = Product::findOrFail($id);
+
+        //Eliminacion del producto
+        $product->delete();
+
+        //Redireccion con un mensaje flash de sesion
+        return redirect()->route('products.index')->with('status', 'Producto eliminado satifactoriamente!');
+    }
+    
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return view('products.show', ['product'=>$product]);
     }
 
 }
