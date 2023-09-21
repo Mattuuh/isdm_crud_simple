d868a60cf75f4838e11775cf701a2e069e1bd172
dgarcia843 <142177550+dgarcia843@users.noreply.github.com>
2023-09-20 23:05:58 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,13 @@ Route::get('/', function () {
 });
 
 Route::get('/products',[ProductController::class,'index'])->name('products.index');
-// route store
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create'); 
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');// route store
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create'); // route create
 Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show'); // route show
-// route update
+Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');// route update
 Route::delete('(products/{product}', [   ProductController::class, 'destroy'])->name('products.destroy'); // route destroy
-// route edit
-Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');// route edit
+
+
+
 
