0e82c7c3545f81c7b6374d29e10762128c5a6657
“Matias” <cisnerosmatias394@gmail.com>
2023-09-27 20:36:08 -0300

    Agregado laravel ui y socialite

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name
 
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
