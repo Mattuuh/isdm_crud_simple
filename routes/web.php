f84ff32f6292635d5cdc0f55d31f80665737152f
Mattuuh <99518568+Mattuuh@users.noreply.github.com>
2023-09-27 23:53:21 -0300

    Correciones y proteccion a rutas

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@ use App\Http\Controllers\ProductController;
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\GoogleController;
+use App\Http\Middleware\Authenticate;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,18 +20,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products',[ProductController::class,'index'])->name('products.index');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');// route store
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create'); // route create
-Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show'); // route show
-Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');// route update
-Route::delete('(products/{product}', [   ProductController::class, 'destroy'])->name('products.destroy'); // route destroy
-Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');// route edit
 Route::get('auth/google',[GoogleController::class,'redirectToGoogle']);
 Route::get('/google/callback',[GoogleController::class,'handleGoogleCallback']);
 
-
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/products',[ProductController::class,'index'])->name('products.index')->middleware(Authenticate::class);
+Route::post('/products', [ProductController::class, 'store'])->name('products.store')->middleware(Authenticate::class);// route store
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create')->middleware(Authenticate::class); // route create
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show')->middleware(Authenticate::class); // route show
+Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update')->middleware(Authenticate::class);// route update
+Route::delete('(products/{product}', [   ProductController::class, 'destroy'])->name('products.destroy')->middleware(Authenticate::class); // route destroy
+Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit')->middleware(Authenticate::class);// route edit
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(Authenticate::class);
